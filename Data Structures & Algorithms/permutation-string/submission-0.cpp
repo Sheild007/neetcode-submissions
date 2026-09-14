1d9e84d2e4a7e58d63926000783caec077c80077
Usman Muneer <usman.muneer720@gmail.com>
2026-09-14 23:09:04 +0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        vector<int> s1_freq(26, 0);
+        vector<int> s2_freq(26, 0);
+
+        for (char c : s1)
+            s1_freq[c - 'a']++;
+
+        int l = 0;
+
+        for (int r = 0; r < s2.size(); r++) {
+            s2_freq[s2[r] - 'a']++;
+            if (r - l + 1 > s1.size()) {
+                s2_freq[s2[l] - 'a']--;
+                l++;
+            }
+            if (r - l + 1 == s1.size()) {
+                if (s1_freq == s2_freq)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
